bd3d4ce55d3db99071e5918e33833d7cbfff86cc
user726687 <98432183+user726687@users.noreply.github.com>
2024-11-23 03:45:44 +0800

    auto-find java path

--- minecraft.cmd
+++ minecraft.cmd
@@ -1,7 +1,7 @@
 set jdk_url=https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.zip
 set pmc_url=https://github.com/fn2006/PollyMC/releases/download/8.0/PollyMC-Windows-MSVC-Portable-8.0.zip
 (
-    start "download jdk" cmd /C "curl --create-dirs -o Minecraft/jre/jdk.zip %jdk_url% -J -L && tar -xf Minecraft/jre/jdk.zip --strip-components=1 --directory=Minecraft/jre && del Minecraft\jre\jdk.zip"
-    start "download pmc" cmd /C "curl --create-dirs -o Minecraft/pmc.zip %pmc_url% -J -L && tar -xf Minecraft/pmc.zip --directory=Minecraft && del Minecraft\pmc.zip"
+    start "download jdk" cmd /C "curl --create-dirs -o Minecraft/jre/jdk.zip %jdk_url% -J -L && cd Minecraft/jre && tar -xf jdk.zip --strip-components=1 && del jdk.zip"
+    start "download pmc" cmd /C "curl --create-dirs -o Minecraft/pmc.zip %pmc_url% -J -L && cd Minecraft && tar -xf pmc.zip && del pmc.zip && echo [General]>pollymc.cfg && echo JavaPath=jre/bin/javaw.exe>>pollymc.cfg"
 ) | pause
 exit

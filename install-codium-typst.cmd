24ee9dbd36987c1b1f2dccb1a0fce191b54a3454
user726687 <98432183+user726687@users.noreply.github.com>
2024-12-14 18:01:20 +0800

    Update install-codium-typst.cmd

--- install-codium-typst.cmd
+++ install-codium-typst.cmd
@@ -1,1 +1,1 @@
-winget install VSCodium.VSCodium & codium --install-extension tomoki1207.pdf --install-extension myriad-dreamin.tinymist 
+winget install VSCodium.VSCodium & start "codium --install-extension tomoki1207.pdf --install-extension myriad-dreamin.tinymist"
